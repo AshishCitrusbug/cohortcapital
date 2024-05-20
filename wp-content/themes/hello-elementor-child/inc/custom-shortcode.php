eb706c72332c2b11bb61b516d3b0a69816859401
Ashish Rathod <ashish1.citrusbug@gmail.com>
2024-05-20 12:52:59 +0530

    Completed the Home page
    Completed the home page and made CSS changes for whole site of pages in CSS files.

--- wp-content/themes/hello-elementor-child/inc/custom-shortcode.php
+++ wp-content/themes/hello-elementor-child/inc/custom-shortcode.php
@@ -13,23 +13,29 @@ function team_member_shortcode() {
   if ( $team_query->have_posts() ) {
     $team_output = '
     <section class="team">
-      <div class="team__main">
-        <div class="team__main--inner">
-          <div class="team__main--inner-title">
-            <h2>'.$team_main_title.'</h2>
-            <p>'.$team_main_content.'</p>
+      <div class="team__main" id="team__main">
+        <div class="team__main--inner " data-aos="fade-up" data-aos-duration="2000">
+          <div class="team__main--inner-title ">
+            <h2 class="">'.$team_main_title.'</h2>
+            <p class="">'.$team_main_content.'</p>
           </div>
         </div>';
+        $i = 600;
+        $j = 1;
     while ( $team_query->have_posts() ) {
       $team_query->the_post();
       $team_designation = get_field('designation');
       $team_output .= '
-      <div class="team__main--inner">
-        <div class="team__main--inner-main">
-          <div class="team__main--inner-img">
+      <div class="team__main--inner "  data-aos="fade-up" data-aos-once="true" data-aos-duration="1500"';
+      
+      if ($j < 4) {
+        $team_output .= ' data-aos-delay="' . $i . '"';
+    }
+    $team_output .= '><div class="team__main--inner-main ">
+          <div class="team__main--inner-img ">
             <img src="'.get_the_post_thumbnail_url().'" alt="'.get_the_title().'" />
           </div>
-          <div class="team__main--inner-author">
+          <div class="team__main--inner-author ">
             <h4>'.get_the_title().'</h4>
             <p>'.$team_designation.'</p>
           </div>
@@ -42,6 +48,13 @@ function team_member_shortcode() {
           </div>
         </div>
       </div>';  
+      $i = $i + 300;
+      if ($j % 4 == 0) {
+        $j = 1; // Reset $j to 1 after every 4th item
+        $i = 600;
+      } else {
+          $j++; // Increment $j otherwise
+      }
     }
     $team_output .= '</div></section>';
   }
@@ -67,39 +80,40 @@ function transactions_field_loop()
         $loan             = get_field('loan');
         $term_outstanding = get_field('term_outstanding');
         $ltv              = get_field('ltv'); ?>
-    
-      <div class="transactions">
-        <div class="transactions__main">
-          <div class="transactions__title">
-            <h4><?php the_title(); ?></h4>
-          </div>
-          <div class="transactions__main--cont">
-              <?php if (!empty($loan)) { ?>
-                  <div class="transactions__main--cont-data">
-                      <span class="transactions__main--cont-data--title">Loan:</span>
-                      <span class="transactions__main--cont-data--value"><?php echo $loan; ?></span>
-                  </div>
-              <?php }; ?>
-              <?php if (!empty($term_outstanding)) { ?>
-                  <div class="transactions__main--cont-data">
-                      <span class="transactions__main--cont-data--title">Term outstanding:</span>
-                      <span class="transactions__main--cont-data--value"><?php echo $term_outstanding; ?></span>
-                  </div>
-              <?php } ?>
-              <?php if (!empty($ltv)) { ?>
-                  <div class="transactions__main--cont-data">
-                      <span class="transactions__main--cont-data--title">LTV:</span>
-                      <span class="transactions__main--cont-data--value"><?php echo $ltv; ?></span>
-                  </div>
-              <?php } ?>
-          </div>
-        </div>
-        <div class="transactions__main--arrow">
-          <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/trans-arrow.svg'); ?>" alt="trans-arrow">
-        </div>
-      </div>
 
-      <?php wp_reset_postdata();
+<div class="transactions">
+    <div class="transactions__main">
+        <div class="transactions__title">
+            <h4><?php the_title(); ?></h4>
+        </div>
+        <div class="transactions__main--cont">
+            <?php if (!empty($loan)) { ?>
+            <div class="transactions__main--cont-data">
+                <span class="transactions__main--cont-data--title">Loan:</span>
+                <span class="transactions__main--cont-data--value"><?php echo $loan; ?></span>
+            </div>
+            <?php }; ?>
+            <?php if (!empty($term_outstanding)) { ?>
+            <div class="transactions__main--cont-data">
+                <span class="transactions__main--cont-data--title">Term outstanding:</span>
+                <span class="transactions__main--cont-data--value"><?php echo $term_outstanding; ?></span>
+            </div>
+            <?php } ?>
+            <?php if (!empty($ltv)) { ?>
+            <div class="transactions__main--cont-data">
+                <span class="transactions__main--cont-data--title">LTV:</span>
+                <span class="transactions__main--cont-data--value"><?php echo $ltv; ?></span>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
+    <div class="transactions__main--arrow">
+        <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/trans-arrow.svg'); ?>"
+            alt="trans-arrow">
+    </div>
+</div>
+
+<?php wp_reset_postdata();
     }
     return ob_get_clean();
 }
