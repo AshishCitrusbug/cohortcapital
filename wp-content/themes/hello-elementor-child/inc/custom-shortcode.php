858af4c4d87aa7cfe6e1579ac4eee313865ae89b
Ashish Rathod <ashish1.citrusbug@gmail.com>
2024-06-03 14:24:18 +0530

    Updated the font and fixes the safari issues

--- wp-content/themes/hello-elementor-child/inc/custom-shortcode.php
+++ wp-content/themes/hello-elementor-child/inc/custom-shortcode.php
@@ -1,111 +1,116 @@
 <?php
-function team_member_shortcode() { 
-  
-  $team_args = array(
-    'post_type' => 'team',
-    'posts_per_page' => -1,
-  );
-  $team_query = new WP_Query( $team_args ); 
+function team_member_shortcode()
+{
 
-  $team_main_title = get_field('our_team_title',95);
-  $team_main_content = get_field('our_team_content',95);
-  
-  if ( $team_query->have_posts() ) {
-    $team_output = '
+    $team_args = array(
+        'post_type' => 'team',
+        'posts_per_page' => -1,
+    );
+    $team_query = new WP_Query($team_args);
+
+    $team_main_title = get_field('our_team_title', 95);
+    $team_main_content = get_field('our_team_content', 95);
+
+    if ($team_query->have_posts()) {
+        $team_output = '
     <section class="team">
       <div class="team__main" id="team__main">
         <div class="team__main--inner " data-aos="fade-up" data-aos-duration="1500">
           <div class="team__main--inner-title ">
-            <h2 class="">'.$team_main_title.'</h2>
-            <p class="">'.$team_main_content.'</p>
+            <h2 class="">' . $team_main_title . '</h2>
+            <p class="">' . $team_main_content . '</p>
           </div>
         </div>';
         $i = 600;
         $j = 1;
-    while ( $team_query->have_posts() ) {
-      $team_query->the_post();
-      $team_designation = get_field('designation');
-      $team_output .= '
+        while ($team_query->have_posts()) {
+            $team_query->the_post();
+            $team_designation = get_field('designation');
+            $team_output .= '
       <div class="team__main--inner "  data-aos="fade-up" data-aos-once="true" data-aos-duration="1500"';
-      
-      if ($j < 4) {
-        $team_output .= ' data-aos-delay="' . $i . '"';
-    }
-    $team_output .= '><div class="team__main--inner-main ">
+
+            if ($j < 4) {
+                $team_output .= ' data-aos-delay="' . $i . '"';
+            }
+            $team_output .= '><div class="team__main--inner-main ">
           <div class="team__main--inner-img ">
-            <img src="'.get_the_post_thumbnail_url().'" alt="'.get_the_title().'" />
+            <img src="' . get_the_post_thumbnail_url() . '" alt="' . get_the_title() . '" />
           </div>
           <div class="team__main--inner-author ">
-            <h4>'.get_the_title().'</h4>
-            <p>'.$team_designation.'</p>
+            <h4>' . get_the_title() . '</h4>
+            <p>' . $team_designation . '</p>
           </div>
           <div class="team__main--inner-over">
-            <div class="team__main--inner-over--cont">'.apply_filters('the_content', get_the_content()).'</div>
+            <div class="team__main--inner-over--cont">' . apply_filters('the_content', get_the_content()) . '</div>
             <div class="team__main--inner-over--author">
-              <h3>'.get_the_title().'</h3>
-              <p>'.$team_designation.'</p>
+              <h3>' . get_the_title() . '</h3>
+              <p>' . $team_designation . '</p>
             </div>
           </div>
         </div>
-      </div>';  
-      $i = $i + 300;
-      if ($j % 4 == 0) {
-        $j = 1; // Reset $j to 1 after every 4th item
-        $i = 600;
-      } else {
-          $j++; // Increment $j otherwise
-      }
+      </div>';
+            $i = $i + 300;
+            if ($j % 4 == 0) {
+                $j = 1; // Reset $j to 1 after every 4th item
+                $i = 600;
+            } else {
+                $j++; // Increment $j otherwise
+            }
+        }
+        $team_output .= '</div></section>';
     }
-    $team_output .= '</div></section>';
-  }
-  // Restore original Post Data.
-  wp_reset_postdata($team_output);
+    // Restore original Post Data.
+    wp_reset_postdata($team_output);
 
-  return $team_output;
-
+    return $team_output;
 }
-add_shortcode( 'team_member', 'team_member_shortcode' );
+add_shortcode('team_member', 'team_member_shortcode');
 function transactions_field_loop()
 {
-   
+
+    $loan_num             = get_field('loan_num');
     $loan             = get_field('loan');
+    $term_outstanding_num = get_field('term_outstanding_num');
     $term_outstanding = get_field('term_outstanding');
-    $ltv              = get_field('ltv'); ?>
+    $ltv_num              = get_field('ltv_num');
+    $ltv              = get_field('ltv');
 
-<div class="transactions">
-    <div class="transactions__main">
-        <div class="transactions__title">
-            <h4><?php the_title(); ?></h4>
+
+?>
+
+    <div class="transactions">
+        <div class="transactions__main">
+            <div class="transactions__title">
+                <h4><?php the_title(); ?></h4>
+            </div>
+            <div class="transactions__main--cont">
+                <?php if (!empty($loan)) { ?>
+                    <div class="transactions__main--cont-data">
+                        <span class="transactions__main--cont-data--title">Loan:</span>
+                        <span class="transactions__main--cont-data--value"><?php echo $loan_num; ?> <span><?php echo $loan; ?></span></span>
+                    </div>
+                <?php }; ?>
+                <?php if (!empty($term_outstanding)) { ?>
+                    <div class="transactions__main--cont-data">
+                        <span class="transactions__main--cont-data--title">Term:</span>
+                        <span class="transactions__main--cont-data--value"><?php echo $term_outstanding_num; ?><span><?php echo $term_outstanding; ?></span></span>
+                    </div>
+                <?php } ?>
+                <?php if (!empty($ltv)) { ?>
+                    <div class="transactions__main--cont-data">
+                        <span class="transactions__main--cont-data--title">LTV:</span>
+                        <span class="transactions__main--cont-data--value"><?php echo $ltv_num; ?><span><?php echo $ltv; ?></span></span>
+                    </div>
+                <?php } ?>
+            </div>
         </div>
-        <div class="transactions__main--cont">
-            <?php if (!empty($loan)) { ?>
-            <div class="transactions__main--cont-data">
-                <span class="transactions__main--cont-data--title">Loan:</span>
-                <span class="transactions__main--cont-data--value"><?php echo $loan; ?></span>
-            </div>
-            <?php }; ?>
-            <?php if (!empty($term_outstanding)) { ?>
-            <div class="transactions__main--cont-data">
-                <span class="transactions__main--cont-data--title">Term:</span>
-                <span class="transactions__main--cont-data--value"><?php echo $term_outstanding; ?></span>
-            </div>
-            <?php } ?>
-            <?php if (!empty($ltv)) { ?>
-            <div class="transactions__main--cont-data">
-                <span class="transactions__main--cont-data--title">LTV:</span>
-                <span class="transactions__main--cont-data--value"><?php echo $ltv; ?></span>
-            </div>
-            <?php } ?>
-        </div>
-    </div>
-    <div class="transactions__main--arrow">
-        <a href="<?php echo get_permalink(); ?>"> <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/trans-arrow.svg'); ?>"
-            alt="trans-arrow">
+        <div class="transactions__main--arrow">
+            <a href="<?php echo get_permalink(); ?>"> <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/trans-arrow.svg'); ?>" alt="trans-arrow">
             </a>
+        </div>
     </div>
-</div>
 
-<?php
+    <?php
 
 }
 add_shortcode('transactions_details', 'transactions_field_loop');
@@ -114,82 +119,82 @@ add_shortcode('transactions_details', 'transactions_field_loop');
 
 function testimonial_custom_loop($atts)
 {
-    ob_start(); 
-    
+    ob_start();
+
     $args = array(
         'post_type' => 'testimonials',
         'posts_per_page' => -1,
     );
-    
+
     $testimonial_query = new WP_Query($args);
-    
+
     if ($testimonial_query->have_posts()) {
-        ?>
+    ?>
         <div class="testimonial" data-testimonial>
             <div class="testimonial__main owl-carousel owl-theme">
-                <?php 
-                $i = 0; 
-                while ($testimonial_query->have_posts()) : $testimonial_query->the_post(); 
+                <?php
+                $i = 0;
+                while ($testimonial_query->have_posts()) : $testimonial_query->the_post();
                     $title = get_the_title();
                     $content = get_the_content();
                     $designation = get_field('testimonials_designation');
                     $company = get_field('testimonials_company');
-                    $image_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); 
-                    $rating = get_field('testimonial_rating'); 
+                    $image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                    $rating = get_field('testimonial_rating');
 
                     if ($i % 2 == 0) {
                         echo '<div class="testimonial__main--item">';
                     }
-                    ?>
-                        <div class="testimonial__main--item-top">
-                            <div class="testimonial__main--item-top-cont">
-                                <?php if ($image_url): ?>
-                                    <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($title); ?>">
-                                <?php endif; ?>
-                                <?php if ($rating){ ?>
-                                    <ul>
-                                        <?php for ($j = 0; $j < $rating; $j++): ?>
-                                            <li>
-                                                <a href="#">
-												    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/testinomial-rating.svg'); ?>" alt="Star Rating">
-                                                </a>
-                                            </li>
-                                        <?php endfor; ?>
-                                    </ul>
-                                <?php };
-								if (!empty($content)){ 
-								?>
-                                   <p><?php echo $content; ?></p>
-								<?php }; ?>
-                            </div>
-                            <div class="testimonial__main--item-top-auth">
-                                <h4><?php echo $title; ?></h4>
-                                <?php if(!empty($designation)) { ?>
-                                    <p><?php echo $designation; ?></p>
-                                <?php } 
-                                if(!empty($company)){ ?>
-                                    <p><?php echo $company; ?></p>
-                                <?php } ?>
-                            </div>
+                ?>
+                    <div class="testimonial__main--item-top">
+                        <div class="testimonial__main--item-top-cont">
+                            <?php if ($image_url) : ?>
+                                <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($title); ?>">
+                            <?php endif; ?>
+                            <?php if ($rating) { ?>
+                                <ul>
+                                    <?php for ($j = 0; $j < $rating; $j++) : ?>
+                                        <li>
+                                            <a href="#">
+                                                <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/testinomial-rating.svg'); ?>" alt="Star Rating">
+                                            </a>
+                                        </li>
+                                    <?php endfor; ?>
+                                </ul>
+                            <?php };
+                            if (!empty($content)) {
+                            ?>
+                                <p><?php echo $content; ?></p>
+                            <?php }; ?>
                         </div>
-                    <?php 
+                        <div class="testimonial__main--item-top-auth">
+                            <h4><?php echo $title; ?></h4>
+                            <?php if (!empty($designation)) { ?>
+                                <p><?php echo $designation; ?></p>
+                            <?php }
+                            if (!empty($company)) { ?>
+                                <p><?php echo $company; ?></p>
+                            <?php } ?>
+                        </div>
+                    </div>
+                <?php
                     $i++;
                     if ($i % 2 == 0) {
-                        echo '</div>'; 
+                        echo '</div>';
                     }
                 endwhile;
 
                 if ($i % 2 != 0) {
-                    echo '</div>'; 
+                    echo '</div>';
                 }
                 ?>
             </div>
         </div>
-        <?php
+    <?php
     }
-    
+
     wp_reset_postdata();
-    
+
     return ob_get_clean();
 }
 
@@ -200,69 +205,69 @@ add_shortcode('testimonial_custom_loop_section', 'testimonial_custom_loop');
 
 function testimonial_home_page($atts)
 {
-	ob_start(); 
-    
+    ob_start();
+
     $args = array(
         'post_type' => 'testimonials',
         'posts_per_page' => -1,
     );
-    
+
     $testimonial_query = new WP_Query($args);
-    
+
     if ($testimonial_query->have_posts()) {
-        ?>
+    ?>
         <div class="testimonial" data-testimonial>
             <div class="testimonial__main owl-carousel owl-theme">
                 <?php while ($testimonial_query->have_posts()) : $testimonial_query->the_post(); ?>
                     <div class="testimonial__main--item">
                         <?php
-                            $title = get_the_title();
-                            $content = get_the_content();
-                            $designation = get_field('testimonials_designation');
-                            $company = get_field('testimonials_company');
-                            $image_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); 
-							$rating = get_field('testimonial_rating'); 
+                        $title = get_the_title();
+                        $content = get_the_content();
+                        $designation = get_field('testimonials_designation');
+                        $company = get_field('testimonials_company');
+                        $image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                        $rating = get_field('testimonial_rating');
                         ?>
                         <div class="testimonial__main--item-top">
                             <div class="testimonial__main--item-top-cont">
-                                <?php if ($image_url): ?>
+                                <?php if ($image_url) : ?>
                                     <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($title); ?>">
                                 <?php endif; ?>
-								<?php if (!empty($rating)){ ?>
+                                <?php if (!empty($rating)) { ?>
                                     <ul>
-                                        <?php for ($i = 0; $i < $rating; $i++): ?>
+                                        <?php for ($i = 0; $i < $rating; $i++) : ?>
                                             <li>
                                                 <a href="#">
-												<img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/testinomial-rating.svg'); ?>" alt="Star Rating">
+                                                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/testinomial-rating.svg'); ?>" alt="Star Rating">
                                                 </a>
                                             </li>
                                         <?php endfor; ?>
                                     </ul>
-                                <?php }; 
-								 if (!empty($content)){ 
-								?>
-                                   <p><?php echo $content; ?></p>
-								<?php }; ?>
+                                <?php };
+                                if (!empty($content)) {
+                                ?>
+                                    <p><?php echo $content; ?></p>
+                                <?php }; ?>
                             </div>
                             <div class="testimonial__main--item-top-auth">
                                 <h4><?php echo $title; ?></h4>
-								<?php if(!empty($designation)) { ?>
+                                <?php if (!empty($designation)) { ?>
                                     <p><?php echo $designation; ?></p>
-								<?php } 
-								 if(!empty($company)){ ?>
-                                     <p><?php echo $company; ?></p>
-								<?php } ?>
+                                <?php }
+                                if (!empty($company)) { ?>
+                                    <p><?php echo $company; ?></p>
+                                <?php } ?>
                             </div>
                         </div>
                     </div>
                 <?php endwhile; ?>
             </div>
         </div>
-        <?php
+    <?php
     }
-    
+
     wp_reset_postdata();
-    
+
     return ob_get_clean();
 }
 
@@ -272,10 +277,51 @@ add_shortcode('testimonial_home_page_section', 'testimonial_home_page');
 
 function insights_article_search($atts)
 {
-    ob_start(); 
-    
+    ob_start();
+    ?>
 
-return ob_get_clean();
+    <div class="insights" id="ajaxcontainercust">
+        <div class="insights__main"> 
+            <?php
+            $args = array(
+                'post_type' => 'article',
+                'posts_per_page' => -1,
+                'post_status' => 'publish',
+                'order' => 'DESC',
+            );
+            
+            $query = new WP_Query($args);
+            if ($query->have_posts()) :
+                while ($query->have_posts()) : $query->the_post(); ?>
+                    <div class="insights__main--box">
+                        <div class="insights__main--box-img">
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+                        </div>
+                        <div class="insights__main--box-cont">
+                            <div class="insights__main--box-cont--title">
+                                <h4><?php echo get_the_title(); ?></h4>
+                                <span>2 min read</span>
+                            </div>
+                            <div class="insights__main--box-cont--date">
+                                <span><?php echo get_the_date(); ?>, by <?php echo get_the_author(); ?></span>
+                            </div>
+                            <div class="insights__main--box-cont--content">
+                                <p><?php echo get_the_excerpt(); ?></p>
+                            </div>
+                            <a href="<?php echo get_the_permalink(); ?>" class="insights__main--box-cont--link">
+                                Read Article
+                                <img src="/cohortcapital/wp-content/uploads/2024/05/post-arrow.svg" alt="">
+                            </a>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            <?php endif; ?>
+        </div>
+    </div>
+
+<?php
+    return ob_get_clean();
 }
 
 add_shortcode('insights_page_article_search_section', 'insights_article_search');
---
+++ wp-content/uploads/smush/smush-log.php
@@ -0,0 +1,22 @@
+<?php die(); ?>
+[2024-05-29T12:59:29+00:00] Info: Background wp_smush_background_scan_process: Spawning a brand new instance (ID: edef66a56d13a5ac508b92903f173eca) for the process.
+[2024-05-29T12:59:29+00:00] Info: Background wp_smush_background_scan_process: Dispatching a new request for instance edef66a56d13a5ac508b92903f173eca.
+[2024-05-29T12:59:29+00:00] Info: Background wp_smush_background_scan_process: Scheduling new event with hook wp_smush_background_scan_process_cron to run wp_smush_background_scan_process_cron_interval.
+[2024-05-29T12:59:30+00:00] Info: Background wp_smush_background_scan_process: Setting last run timestamp for instance ID edef66a56d13a5ac508b92903f173eca to 2024-05-29 12:59:30
+[2024-05-29T12:59:30+00:00] Info: Background wp_smush_background_scan_process: Executing task 1.
+[2024-05-29T12:59:30+00:00] Error: Tried to check animated value but encountered an problem with the media item
+[2024-05-29T12:59:30+00:00] Error: Tried to check animated value but encountered an problem with the media item
+[2024-05-29T12:59:30+00:00] Info: Background wp_smush_background_scan_process: Process completed.
+[2024-05-29T12:59:30+00:00] Info: Background wp_smush_background_scan_process: Cancelling event with hook wp_smush_background_scan_process_cron.
+[2024-05-29T12:59:47+00:00] Info: Background wp_smush_background_scan_process: Spawning a brand new instance (ID: a732779a64a99a6ca135b2eb5664f011) for the process.
+[2024-05-29T12:59:47+00:00] Info: Background wp_smush_background_scan_process: Dispatching a new request for instance a732779a64a99a6ca135b2eb5664f011.
+[2024-05-29T12:59:47+00:00] Info: Background wp_smush_background_scan_process: Scheduling new event with hook wp_smush_background_scan_process_cron to run wp_smush_background_scan_process_cron_interval.
+[2024-05-29T12:59:47+00:00] Info: Background wp_smush_background_scan_process: Setting last run timestamp for instance ID a732779a64a99a6ca135b2eb5664f011 to 2024-05-29 12:59:47
+[2024-05-29T12:59:47+00:00] Info: Background wp_smush_background_scan_process: Executing task 1.
+[2024-05-29T12:59:47+00:00] Error: Tried to check animated value but encountered an problem with the media item
+[2024-05-29T12:59:48+00:00] Error: Tried to check animated value but encountered an problem with the media item
+[2024-05-29T12:59:48+00:00] Info: Background wp_smush_background_scan_process: Process completed.
+[2024-05-29T12:59:48+00:00] Info: Background wp_smush_background_scan_process: Cancelling event with hook wp_smush_background_scan_process_cron.
+[2024-05-30T06:30:07+00:00] Error: Count not delete webp versions of the media item [2659]
+[2024-05-30T07:16:29+00:00] Error: Count not delete webp versions of the media item [2660]
+[2024-05-31T05:59:48+00:00] Error: Count not delete webp versions of the media item [2739]
